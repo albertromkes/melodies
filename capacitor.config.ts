ad6faed28ca555390b3c1322ef2b592475809bc4
albert romkes <albert@rainology.nl>
2026-01-06 15:45:14 +0100

    updated icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'nl.psalmmelodies.app',
-  appName: 'Psalm Melodies',
+  appName: 'Psalmen en liederen',
   webDir: 'dist',
   server: {
     // For development, you can enable this to load from your dev server
